ecb2b8d268a45c40b232c69f4ef96f5fd7021676
Muhammad Haekal <93haekal@gmail.com>
2018-09-12 10:09:56 +0700

    Fixing bug

--- api/accounts/controller.js
+++ api/accounts/controller.js
@@ -3,7 +3,7 @@ const models = require('./../models')
 module.exports= {
     get: (req,res) => {
         models.accounts.findAll().then(account => {
-            if(account===nul){
+            if(account===null){
                 return res.send({
                     message: "Data not found"
                 })
